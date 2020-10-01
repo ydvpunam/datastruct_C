3b08b8bbfb952498f3ad25a80019e95ecfdaefa4
kpraween10 <56594467+kpraween10@users.noreply.github.com>
2020-10-01 06:55:32 +0530

    Update sizedata.c

--- sizedata.c
+++ sizedata.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-	
+//printing the size of data type	
 int main() {
-printf("int		 %d bytes\n",sizeof(void))
-printf("char 	 %d bytes\n",sizeof(char))
-printf("short	 %d bytes\n",sizeof(short))
-printf("int 	 %d bytes\n",sizeof(int))
-printf("float  	 %d bytes\n",sizeof(float))
-printf("long 	 %d bytes\n",sizeof(long))
-printf("double	 %d bytes\n",sizeof(double))
-printf("longlong	%d bytes\n",sizeof(long long))
-printf("longdble	%d bytes\n",sizeof(long double))
-
+printf("int		 %d bytes\n",sizeof(void));
+printf("char 	 %d bytes\n",sizeof(char));
+printf("short	 %d bytes\n",sizeof(short));
+printf("int 	 %d bytes\n",sizeof(int));
+printf("float  	 %d bytes\n",sizeof(float));
+printf("long 	 %d bytes\n",sizeof(long));
+printf("double	 %d bytes\n",sizeof(double));
+printf("longlong	%d bytes\n",sizeof(long long));
+printf("longdble	%d bytes\n",sizeof(long double));
     return 0;
 }
