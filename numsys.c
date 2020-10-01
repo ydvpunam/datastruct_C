9e23f713b87af125cd16f3493ea532b7a00f9f4c
kpraween10 <56594467+kpraween10@users.noreply.github.com>
2020-10-01 06:46:26 +0530

    Update numsys.c

--- numsys.c
+++ numsys.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 int main(){
-	 unsigned char a=69;
-	print("%d\t",a);//decimal print
-	print("%c\t",a);//character print
-	print("0x%x\t",a);//hexadecimal
-	print("0%o\t",a);//octal
-	print("%b\t",a);//binary
+	unsigned char c=0x6;//hexadecimal
+	unsigned char b=09;//octal
+	 unsigned char a=69;//full 8bits is free no bit for sign
+	printf("%d\t",a);//decimal print
+	printf("%c\t",a);//character print
+	printf("0x%x\t",a);//hexadecimal
+	printf("0%o\t",a);//octal
+	//printf("%b\t",a);//binary//this may not work
 	return 0;
 }
